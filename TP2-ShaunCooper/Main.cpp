1ca4590f6324c31ac5f3454f8b23de3af7cb1d78
manoslive <ebeloin@hotmail.com>
2014-11-13 13:28:32 -0500

    qaasd

--- TP2-ShaunCooper/Main.cpp
+++ TP2-ShaunCooper/Main.cpp
@@ -3,7 +3,7 @@
 	TP2 - File d'attente
 	dernière modification : 13-11-14
 	Main.cpp
-	Programme qui permet de gérer un file d'attente d'un restaurent
+	Programme qui permet de gérer un file d'attente d'un restaurent qwerty
 */
 
 #include "ClientEnAttente.h"
