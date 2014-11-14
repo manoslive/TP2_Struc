12cad17a6e5ed856ebc3ad0014b22c9b38f797eb
manoslive <ebeloin@hotmail.com>
2014-11-14 16:47:07 -0500

    Commentaires part 8

--- TP2-ShaunCooper/ClientEnAttente.cpp
+++ TP2-ShaunCooper/ClientEnAttente.cpp
@@ -19,7 +19,7 @@ ClientEnAttente::ClientEnAttente()
 }
 ClientEnAttente::ClientEnAttente(string nom, int nbDePersonnes, int sectionsChoisie)
 {
-	if (sectionsChoisie > 0 && sectionsChoisie < 8)
+	if (sectionsChoisie > 0 && sectionsChoisie < 8) // Le choix est valide
 	{
 		SetNom(nom);
 		SetSection(sectionsChoisie);
@@ -27,7 +27,7 @@ ClientEnAttente::ClientEnAttente(string nom, int nbDePersonnes, int sectionsChoi
 		SetPrecedent(nullptr);   // 0 indique qu'on ne connait pas l'adresse; equivaut à null
 		SetSuivant(nullptr);
 	}
-	else
+	else // Le choix est invalide, on lance une exception
 		cout << "La section choisie est invalide. Choix: salle(0), terrasse(1), terrasseFumeur(2)" << endl;
 }
 void ClientEnAttente::SetPrecedent(ClientEnAttente * p)
@@ -48,9 +48,9 @@ ClientEnAttente * ClientEnAttente::GetSuivant() const
 }
 void ClientEnAttente::SetNom(string nom)
 {
-	if (nom != "")
+	if (nom != "") // Le choix est valide
 		InfoClient.nom_ = nom;
-	else
+	else // Le choix est invalide, on lance une exception
 		throw exception("Erreur: nom invalide. Veuillez entrer un nom");
 }
 string ClientEnAttente::GetNom() const
@@ -59,16 +59,16 @@ string ClientEnAttente::GetNom() const
 }
 void ClientEnAttente::SetNombreDePersonnes(int nbDePersonnes)
 {
-	if (nbDePersonnes > 0)
+	if (nbDePersonnes > 0) // Le choix est valide
 		InfoClient.nombreDePersonnes = nbDePersonnes;
-	else
+	else // Le choix est invalide, on lance une exception
 		throw exception("Erreur: nombre de personnes dans le groupe invalide. Le nombre doit être plus grand que 0");
 }
 void ClientEnAttente::SetSection(int sectionChoisie)
 {
-	if (sectionChoisie > 0 && sectionChoisie < 8)
+	if (sectionChoisie > 0 && sectionChoisie < 8) // Le choix est valide
 		InfoClient.section = sectionChoisie;
-	else
+	else // Le choix est invalide, on lance une exception
 		throw exception("Erreur: Section invalide!");
 }
 int ClientEnAttente::GetNombreDePersonnes() const
